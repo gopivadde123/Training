52fc4dc5b9a0a18199733f591abfb494e73bec07
vamsiveligatla475 <vamsiveligatla.ip@gmail.com>
2018-11-01 05:50:35 -0700

    new code

--- utils/logInSchema.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose'),
-    Schema = mongoose.Schema,
-    subSchema = new Schema({
-        'user_name':{type:String},
-        'password' :{type: String},
-        'valid' :{type:String}
-    },{strict:true});
-module.exports=subSchema;
